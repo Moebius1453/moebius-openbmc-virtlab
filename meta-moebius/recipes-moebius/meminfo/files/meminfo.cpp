a763771dc4273ccb31e9e094f91c3d84adb43613
moebius <1182531447@qq.com>
2026-09-06 07:33:15 +0000

    注册服务

--- meta-moebius/recipes-moebius/meminfo/files/meminfo.cpp
+++
@@ -1,7 +0,0 @@
-#include <sdbusplus/server/object.hpp>
-#include <xyz/moebius/MemoryStats.interface.yaml>
-#include <cstdint>  <fstream>  <string> 
- 
-using MemoryStatsIntf =
-    sdbusplus::xyz::moebius::MemoryStats::server::MemoryStats;
-
