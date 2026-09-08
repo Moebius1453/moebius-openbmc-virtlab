7b552bb249161086e6239450665bc02d2676df59
moebius <1182531447@qq.com>
2026-09-08 05:48:52 +0000

    memwatcher完成

--- meta-moebius/recipes-moebius/memwatcher/files/main.cpp
+++ meta-moebius/recipes-moebius/memwatcher/files/main.cpp
@@ -1,19 +1,19 @@
 #include <sdbusplus/bus.hpp>
 #include <sdbusplus/server/manager.hpp>
 
-#include "wather.hpp"
+#include "watcher.hpp"
 
 int main()
 {
     auto bus = sdbusplus::bus::new_default();
    
-    MemWatcher memwatcher(bus, "/xyz/moebius/memwatcher");
+    MemWatcher memwatcher(bus);
 
     while (true)
     {  
         bus.process_discard();
         bus.wait(5000000);
-        watcher.check()
+        memwatcher.check();
     }
     
     return 0;
